318ae4267b0e5891b252ad5c9b30e8952e6fc453
Van Sever <lennert_vansever@hotmail.be>
2018-10-10 17:47:47 +0200

    making mysql queries

--- data/resolvers.js
+++ data/resolvers.js
@@ -1,16 +1,81 @@
+import { connection } from './connection';
+import { cpus } from 'os';
+
 const resolvers = {
   Post: {
-    Author(){
-      return {
-        first_name: "lennert"
-      }
+    Author({author_id}){
+      return new Promise((resolve, reject) => {
+        const query = {
+          text: "SELECT * FROM graphql_demo.author where id = $1",
+          values: [author_id]
+        }
+        connection.query(query, (error, {rows}) => {
+          if(error) reject(error);
+          resolve(rows[0]);
+        })
+      })
+    },
+    Comments({id}){
+      return new Promise((resolve, reject) => {
+        const query = {
+          text: "SELECT * FROM graphql_demo.comment where post_id = $1",
+          values: [id]
+        }
+        connection.query(query, (error, {rows}) => {
+          if(error) reject(error);
+          resolve(rows);
+        })
+      })
+    }
+  },
+  Author: {
+    Posts({id}){
+      return new Promise((resolve, reject) => {
+        const query = {
+          text: "SELECT * FROM graphql_demo.post where author_id = $1",
+          values: [id]
+        }
+        connection.query(query, (error, {rows}) => {
+          if(error) reject(error);
+          resolve(rows);
+        })
+      })
+    }
+  },
+  Comment: {
+    Post({post_id}){
+      return new Promise((resolve, reject) => {
+        const query = {
+          text: "SELECT * FROM graphql_demo.post where id = $1",
+          values: [post_id]
+        }
+        connection.query(query, (error, {rows}) => {
+          if(error) reject(error);
+          resolve(rows[0]);
+        })
+      })
+    },
+    Author({author_id}){
+      return new Promise((resolve, reject) => {
+        const query = {
+          text: "SELECT * FROM graphql_demo.author where id = $1",
+          values: [author_id]
+        }
+        connection.query(query, (error, {rows}) => {
+          if(error) reject(error);
+          resolve(rows[0]);
+        })
+      })
     }
   },
   Query: {
     posts: () => {
-      return [{
-        title: "my post"
-      }];
+      return new Promise((resolve, reject) => {
+        connection.query("SELECT * FROM graphql_demo.post", (error, {rows}) => {
+          if(error) reject(error);
+          resolve(rows);
+        })
+      })
     }
   }
 }
