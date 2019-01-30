76c29876cd77661f20cd4e24607d738e9686eefa
Lennert Van Sever <lennert.vansever@borealisgroup.com>
2019-01-30 22:48:07 +0100

    back to light

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const server = new ApolloServer({
   },
   playground: {
     settings: {
-      //'editor.theme': 'light',
+      'editor.theme': 'light',
       'editor.cursorShape': 'line'
     },
   }
